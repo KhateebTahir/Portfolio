139e51fff1c4c0299f31a42cf9a7e009d8b8beca
Khateeb Tahir <khateebtahir@Khateebs-iMac.local>
2019-02-01 12:51:31 -0500

    _app.js added

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,17 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 
-import axios from "axios";
+import { Container, Button } from "reactstrap";
 
 class Index extends React.Component {
-  static async getInitialProps() {
-    let userData = {};
-
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      userData = response.data;
-    } catch (err) {
-      console.error(err);
-    }
-    return { userData };
-  }
-
   render() {
-    debugger;
-    const { userData } = this.props;
     return (
       <BaseLayout>
-        <h1>I am Home Page</h1>
-        <h2> {userData.title} </h2>
+        <Container>
+          <Button outline color="danger">
+            danger
+          </Button>
+        </Container>
       </BaseLayout>
     );
   }
